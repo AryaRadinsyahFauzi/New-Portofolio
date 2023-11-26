b7e678fd533825dd7afd397d8e28dd44e3ed9e4c
Arya Radinsyah Fauzi <142789858+AryaRadinsyahFauzi@users.noreply.github.com>
2023-11-26 08:58:00 +0700

    start

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./public/**/*.{html, js}'],
+  theme: {
+    colors: {
+      'background-color' : '#E5E8E9',
+      'primary' : '#FFFFFF',
+      'secondary' : '#006667'
+    },
+    extend: {
+      fontFamily: {
+        poppins: ['poppins'],
+      }
+    },
+  },
+  plugins: [],
+}
+
